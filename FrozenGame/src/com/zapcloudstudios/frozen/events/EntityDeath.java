da7f6c84c7320290c2f3fbf5095c1c07ee82e0a7
SpiderRobotMan <spiderrobotman@gmail.com>
2014-05-16 02:00:06 -0400

    Config File Change

--- FrozenGame/src/com/zapcloudstudios/frozen/events/EntityDeath.java
+++ FrozenGame/src/com/zapcloudstudios/frozen/events/EntityDeath.java
@@ -1,8 +1,13 @@
 package com.zapcloudstudios.frozen.events;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import com.zapcloudstudios.frozen.API;
@@ -11,26 +16,54 @@ import com.zapcloudstudios.frozen.utils.GameManager;
 
 public class EntityDeath implements Listener {
 
-	@EventHandler
-	public void onDeath(EntityDeathEvent e) {
-		if (e.getEntity() instanceof Player) {
-			Player victim = (Player) e.getEntity();
-			if (victim.getKiller() instanceof Player) {
-				Player killer = (Player) victim.getKiller();
+    @SuppressWarnings("deprecation")
+	@EventHandler (priority = EventPriority.LOW, ignoreCancelled = false)
+    public void onSnowballHit(EntityDamageByEntityEvent event)
+    {
+        Entity damaged = event.getEntity();
+        Entity damageEntity = event.getDamager();
+ 
+        if(damaged instanceof Player)
+        if(damageEntity instanceof Snowball)
+        {
+            Snowball snowball = (Snowball)damageEntity;
+            LivingEntity entityThrower = snowball.getShooter();
+            if(entityThrower instanceof Player)
+            {
+                Player playerThrower = (Player)entityThrower;
+                Player playerHit = (Player)damaged;
+                
+                API.addDeaths(playerHit, 1);
+				API.addKills(playerThrower, 1);
+				API.addPoints(playerThrower, 5);
 
-				API.addDeaths(victim, 1);
-				API.addKills(killer, 1);
-				API.addPoints(killer, 5);
-
-				Frozen.scoreboard(killer);
-				Frozen.scoreboard(victim);
-
-				if (!Frozen.gameEnded) {
-					if (API.getKills(killer) >= 5) {
-						GameManager.endGame();
-					}
-				}
-			}
-		}
-	}
+				Frozen.scoreboard(playerThrower);
+				Frozen.scoreboard(playerHit);
+                
+            }
+        }
+    }
+	
+//	@EventHandler
+//	public void onDeath(EntityDeathEvent e) {
+//		if (e.getEntity() instanceof Player) {
+//			Player victim = (Player) e.getEntity();
+//			if (victim.getKiller() instanceof Player) {
+//				Player killer = (Player) victim.getKiller();
+//
+//				API.addDeaths(victim, 1);
+//				API.addKills(killer, 1);
+//				API.addPoints(killer, 5);
+//
+//				Frozen.scoreboard(killer);
+//				Frozen.scoreboard(victim);
+//
+//				if (!Frozen.gameEnded) {
+//					if (API.getKills(killer) >= 5) {
+//						GameManager.endGame();
+//					}
+//				}
+//			}
+//		}
+//	}
 }
